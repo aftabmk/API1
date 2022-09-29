5844f22f6fdda66b04c37d05e1152622dd674c31
Aftab mohammed k <87110394+aftabmk@users.noreply.github.com>
2022-09-29 17:03:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const ax = require("axios");
 const ex = require("express");
+const cors = require('cors')
 
 const url = 'https://kf8gy9csr0.execute-api.ap-south-1.amazonaws.com/webapi/index/topStocksList'
 
@@ -25,8 +26,7 @@ async function f()
 //EXPRESS APP
 
 const app = ex()
-
-
+app.use(cors())
 const port = process.env.PORT || 5000;
 
 app.get(`/${path}`, async (req, res) => {
